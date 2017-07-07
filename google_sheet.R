114e31afca52264f004598ea58b238f1d30ae115
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2017-07-07 02:25:03 -0500

    renew

--- google_sheet.R
+++ google_sheet.R
@@ -6,5 +6,5 @@ gs_ws_ls(ss = atg_lys_ss)
 atg_lys_gene <- gs_read(ss = atg_lys_ss, ws = "atg_lys_complete")
 # rds_03_at_ly_comb_gene_list.rds.gz
 
-readr::write_rds(x = atg_lys_gene, path = "rds_03_at_ly_comb_gene_list.rds.gz")
+readr::write_rds(x = atg_lys_gene, path = "rds_03_a_atg_lys_gene_list.rds.gz")
 
