52d385cf9d5e61f329901fd819eece4e4606251e
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2017-11-03 15:13:55 +0800

    fgsea

--- LearningCodes/MISC/GSEA/clusterProfiler.R
+++ LearningCodes/MISC/GSEA/clusterProfiler.R
@@ -68,6 +68,294 @@ ego3 <- gseGO(geneList     = geneList,
               maxGSSize    = 500,
               pvalueCutoff = 0.05,
               verbose      = FALSE)
+
+geneSets <- DOSE:::getGeneSet(GO_DATA)
+DOSE:::check_gene_id(geneList, geneSets)
+tmp_res <- fgsea::fgsea(pathways=geneSets,
+                 stats=geneList,
+                 nperm=nPerm,
+                 minSize=minGSSize,
+                 maxSize=maxGSSize,
+                 gseaParam=exponent,
+                 nproc = 0)
+p.adj <- p.adjust(tmp_res$pval, method=pAdjustMethod)
+qvalues <- DOSE:::calculate_qvalue(tmp_res$pval)
+Description <- DOSE:::TERM2NAME(tmp_res$pathway, GO_DATA)
+
+params <- list(pvalueCutoff = pvalueCutoff,
+               nPerm = nPerm,
+               pAdjustMethod = pAdjustMethod,
+               exponent = exponent,
+               minGSSize = minGSSize,
+               maxGSSize = maxGSSize
+)
+
+res <- data.frame(
+  ID = as.character(tmp_res$pathway),
+  Description = Description,
+  setSize = tmp_res$size,
+  enrichmentScore = tmp_res$ES,
+  NES = tmp_res$NES,
+  pvalue = tmp_res$pval,
+  p.adjust = p.adj,
+  qvalues = qvalues,
+  stringsAsFactors = FALSE
+)
+res <- res[!is.na(res$pvalue),]
+res <- res[ res$pvalue <= pvalueCutoff, ]
+res <- res[ res$p.adjust <= pvalueCutoff, ]
+idx <- order(res$pvalue, decreasing = FALSE)
+res <- res[idx, ]
+
+if (nrow(res) == 0) {
+  message("no term enriched under specific pvalueCutoff...")
+  return(
+    new("gseaResult",
+        result     = res,
+        geneSets   = geneSets,
+        geneList   = geneList,
+        params     = params,
+        readable   = FALSE
+    )
+  )
+}
+row.names(res) <- res$ID
+
+observed_info <- lapply(geneSets[res$ID], function(gs)
+  DOSE:::gseaScores(geneSet=gs,
+             geneList=geneList,
+             exponent=exponent)
+)
+ledge <- DOSE:::leading_edge(observed_info)
+
+
+# Fgsea -------------------------------------------------------------------
+
+tmp_res <- fgsea(pathways=geneSets,
+                 stats=geneList,
+                 nperm=nPerm,
+                 minSize=minGSSize,
+                 maxSize=maxGSSize,
+                 gseaParam=exponent,
+                 nproc = 0)
+
+pathways = geneSets
+stats = geneList 
+nperm = 1000
+minSize=100 
+maxSize=500
+nproc=0
+gseaParam=1
+BPPARAM=NULL
+
+if (is.null(BPPARAM)) {
+  if (nproc != 0) {
+    if (.Platform$OS.type == "windows") {
+      # windows doesn't support multicore, using snow instead
+      BPPARAM <- SnowParam(workers = nproc)
+    } else {
+      BPPARAM <- MulticoreParam(workers = nproc)
+    }
+  } else {
+    BPPARAM <- BiocParallel:::bpparam()
+  }
+}
+minSize <- max(minSize, 1)
+stats <- sort(stats, decreasing=TRUE)
+stats <- abs(stats) ^ gseaParam
+pathwaysFiltered <-
+  lapply(pathways, function(p) {
+    as.vector(na.omit(fastmatch:::fmatch(p, names(stats))))
+  })
+# pathwaysFiltered is list of the index of stats
+
+pathwaysSizes <- sapply(pathwaysFiltered, length)
+
+toKeep <- which(minSize <= pathwaysSizes & pathwaysSizes <= maxSize)
+m <- length(toKeep)
+
+pathwaysFiltered <- pathwaysFiltered[toKeep]
+npermActual <- nperm
+gseaStatRes <- do.call(rbind,
+                       lapply(
+                         pathwaysFiltered,
+                         calcGseaStat,
+                         stats = stats,
+                         returnLeadingEdge = TRUE
+                       ))
+
+leadingEdges <- mapply("[", list(names(stats)), gseaStatRes[, "leadingEdge"], SIMPLIFY = FALSE)
+pathwayScores <- unlist(gseaStatRes[, "res"])
+
+granularity <- 1000
+permPerProc <- rep(granularity, floor(npermActual / granularity))
+if (npermActual - sum(permPerProc) > 0) {
+  permPerProc <- c(permPerProc, npermActual - sum(permPerProc))
+}
+
+universe <- seq_along(stats)
+seeds <- sample.int(10^9, length(permPerProc))
+# calculate p-value
+counts <- BiocParallel::bplapply(seq_along(permPerProc), function(i) {
+  nperm1 <- permPerProc[i]
+  leEs <- rep(0, m)
+  geEs <- rep(0, m)
+  leZero <- rep(0, m)
+  geZero <- rep(0, m)
+  leZeroSum <- rep(0, m)
+  geZeroSum <- rep(0, m)
+  if (m == 1) {
+    for (i in seq_len(nperm1)) {
+      randSample <- sample.int(length(universe), K)
+      randEsP <- calcGseaStat(
+        stats = stats,
+        selectedStats = randSample,
+        gseaParam = 1)
+      leEs <- leEs + (randEsP <= pathwayScores)
+      geEs <- geEs + (randEsP >= pathwayScores)
+      leZero <- leZero + (randEsP <= 0)
+      geZero <- geZero + (randEsP >= 0)
+      leZeroSum <- leZeroSum + pmin(randEsP, 0)
+      geZeroSum <- geZeroSum + pmax(randEsP, 0)
+    }
+  } else {
+    aux <- fgsea:::calcGseaStatCumulativeBatch(
+      stats = stats,
+      gseaParam = 1,
+      pathwayScores = pathwayScores,
+      pathwaysSizes = pathwaysSizes,
+      iterations = nperm1,
+      seed = seeds[i])
+    leEs = get("leEs", aux)
+    geEs = get("geEs", aux)
+    leZero = get("leZero", aux)
+    geZero = get("geZero", aux)
+    leZeroSum = get("leZeroSum", aux)
+    geZeroSum = get("geZeroSum", aux)
+  }
+  # Time consumption
+  data.table(
+    pathway = seq_len(m),
+    leEs = leEs,
+    geEs = geEs,
+    leZero = leZero,
+    geZero = geZero,
+    leZeroSum = leZeroSum,
+    geZeroSum = geZeroSum
+  )
+}, BPPARAM = BPPARAM)
+
+counts <- rbindlist(counts)
+
+# fgsea algorithm
+# for calcGseaStat-----
+stats <- stats
+# Ordered whole differential gene list
+selectedStats <- pathwaysFiltered[[1]]
+gseaParam = 1
+returnAllExtreme <- F
+returnLeadingEdge <- T
+
+S <- selectedStats # gene set index of stats
+r <- stats # the differential genes
+p <- gseaParam # the power
+
+S <- sort(S)
+m <- length(S)
+N <- length(r)
+
+if (m == N) {
+  stop("GSEA statistic is not defined when all genes are selected")
+  # this is fucking important
+  # you can't use whole gene list as gene set
+  # fucking stupid
+}
+r[S]
+NR <- (sum(abs(r[S]) ^ p))
+rAdj <- abs(r[S]) ^p # rAdj only contains the gene in set S
+if (NR == 0) {
+  # this is equivalent to rAdj being rep(eps, m)
+  rCumSum <- seq_along(rAdj) / length(rAdj)
+} else {
+  rCumSum <- cumsum(rAdj) / NR # divided by NR to normalized to 1
+}
+rCumSum # is the running score.
+
+tops <- rCumSum - (S - seq_along(S)) / (N - m)
+# It's strange to use S minux the index of the S
+# what is the (S - seq_along(S)) / (N - m)
+tops
+
+if (NR == 0) {
+  # this is equivalent to rAdj being rep(eps, m)
+  bottoms <- tops - 1 / m
+} else {
+  bottoms <- tops - rAdj / NR
+}
+maxP <- max(tops)
+minP <- min(bottoms)
+if(maxP > -minP) {
+  geneSetStatistic <- maxP
+} else if (maxP < -minP) {
+  geneSetStatistic <- minP
+} else {
+  geneSetStatistic <- 0
+}
+res <- list(res=geneSetStatistic)
+res <- c(res, list(tops=tops, bottoms=bottoms))
+if (returnLeadingEdge) {
+  leadingEdge <- if (maxP > -minP) {
+    S[seq_along(S) <= which.max(bottoms)]
+  } else if (maxP < -minP) {
+    S[seq_along(S) >= which.min(bottoms)]
+  } else {
+    NULL
+  }
+  
+  res <- c(res, list(leadingEdge=leadingEdge))
+}
+
+# Enrichment score algorithm  in DOSE ---------------------------------------------
+geneSets$`GO:0000139` -> gs
+N <- length(geneList)
+Nh <- length(gs)
+
+Phit <- Pmiss <- numeric(N)
+hits <- names(geneList) %in% gs
+
+Phit[hits] <- abs(geneList[hits])^exponent
+NR <- sum(Phit)
+
+Phit <- cumsum(Phit/NR)
+Pmiss[!hits] <- 1 / (N-Nh)
+Pmiss <- cumsum(Pmiss)
+# This is running enrichment score
+# NR is the sum difference of set
+runningES <- Phit - Pmiss
+## ES is the maximum deviation from zero of Phit-Pmiss
+max.ES <- max(runningES)
+min.ES <- min(runningES)
+if( abs(max.ES) > abs(min.ES) ) {
+  ES <- max.ES
+} else {
+  ES <- min.ES
+}
+
+df <- data.frame(x=seq_along(runningES),
+                 runningScore=runningES,
+                 position=as.integer(hits)
+)
+
+df
+
+df$gene = names(geneList)
+res <- list(ES=ES, runningES = df)
+
+# END -----------------------------------------------------------------------
+
+
+
+
 search_kegg_organism('ece', by='kegg_code')
 ecoli <- search_kegg_organism('Escherichia coli', by='scientific_name')
 dim(ecoli)
