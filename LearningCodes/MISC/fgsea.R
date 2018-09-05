7b970af6bbc80958514b84853dbd9cdf1fe295d4
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2018-09-05 17:43:27 +0800

    fgsea

--- LearningCodes/MISC/fgsea.R
+++ LearningCodes/MISC/fgsea.R
@@ -29,7 +29,15 @@ fgsea::fgsea(
   nperm = 10000
 ) -> rs_fgsea
 
+rs_fgsea[order(pval),] %>% head()
+
+sum(rs_fgsea[, padj < 0.01])
 
 
+plotEnrichment(examplePathways[["5990980_Cell_Cycle"]], exampleRanks)
 
+rs_fgsea[ES > 0][head(order(pval), 10), pathway] -> topPathwaysUp
+rs_fgsea[ES < 0][head(order(pval), 10), pathway] -> topPathwaysDown
+topPathways <- c(topPathwaysUp, rev(topPathwaysDown))
 
+plotGseaTable(examplePathways[topPathways], exampleRanks, rs_fgsea, gseaParam = 0.5)
