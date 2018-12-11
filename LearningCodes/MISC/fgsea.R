b23bdd60877d8d8cfd3040a19a9b80c7971b7e21
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2018-12-11 17:34:08 +0800

    fgsea test

--- LearningCodes/MISC/fgsea.R
+++ LearningCodes/MISC/fgsea.R
@@ -1,12 +1,14 @@
 # fgsea
 
 library(fgsea)
+library(magrittr)
 
 
 # load pathways and gene-level statistics ---------------------------------
 
 
 data("examplePathways")
+data('exampleRanks')
 
 examplePathways %>% 
   purrr::map_int(length) %>% 
@@ -19,6 +21,7 @@ examplePathways %>%
 data("exampleRanks")
 
 exampleRanks %>% head()
+exampleRanks %>% length()
 
 
 fgsea::fgsea(
@@ -31,7 +34,7 @@ fgsea::fgsea(
 
 rs_fgsea[order(pval),] %>% head()
 
-sum(rs_fgsea[, padj < 0.01])
+sum(rs_sfgsea[, padj < 0.01])
 
 
 plotEnrichment(examplePathways[["5990980_Cell_Cycle"]], exampleRanks)
@@ -41,3 +44,21 @@ rs_fgsea[ES < 0][head(order(pval), 10), pathway] -> topPathwaysDown
 topPathways <- c(topPathwaysUp, rev(topPathwaysDown))
 
 plotGseaTable(examplePathways[topPathways], exampleRanks, rs_fgsea, gseaParam = 0.5)
+library(data.table)
+
+reactomePathways(names(exampleRanks)) -> pathways
+fgseaRes <- fgsea(pathways = pathways, exampleRanks, nperm = 1000, maxSize = 500)
+head(fgseaRes)
+
+
+rnk.file <- system.file("extdata", "naive.vs.th1.rnk", package="fgsea")
+gmt.file <- system.file("extdata", "mouse.reactome.gmt", package="fgsea")
+
+
+ranks <- read.table(rnk.file,header=TRUE, colClasses = c("character", "numeric"))
+ranks <- setNames(ranks$t, ranks$ID)
+str(ranks)
+pathways <- gmtPathways(gmt.file)
+str(head(pathways))
+fgseaRes <- fgsea(pathways, ranks, minSize=15, maxSize=500, nperm=1000)
+
