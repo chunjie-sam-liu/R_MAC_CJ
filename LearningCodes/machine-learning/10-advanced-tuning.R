1aa35d5721d9f335e19b833b6c99e5835e30ebe3
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2019-03-17 21:29:03 +0800

    feature selection

--- LearningCodes/machine-learning/10-advanced-tuning.R
+++ LearningCodes/machine-learning/10-advanced-tuning.R
@@ -46,7 +46,33 @@ plotTuneMultiCritResult(res)
 # Feature Selection -------------------------------------------------------
 
 
+# 1. Filtering
+# An external algorithm computes a rank of the variables.
+# Features are subsetted by a certain criteria. he selected features will theb be used to fit a model with optional hyperparameters selected by tuning.
+# 2. feature subset selection
+# No ranking of features is done. Features are selected by a random subset of the data.
+# Then, a model is fit and the performance is chekced. This is done for a lot of feature combinations in a CV setting and the best combination is reported.
+# This method is very computational intense as a lot of models are fitted. Also, strictly all these models would need to be tuned before the performance is estimated which would require an additional nested level in a CV setting.
 
+library(mlr)
+fv = generateFilterValuesData(iris.task, method = "FSelectorRcpp_information.gain")
+fv
+library(FSelector)
+fv2 = generateFilterValuesData(
+  iris.task, 
+  method = c("FSelectorRcpp_information.gain", "FSelector_chi.squared")
+  )
+
+plotFilterValues(fv2) + ggpubr::theme_pubr()
+
+
+# Keep the 2 most important features
+filtered.task = filterFeatures(iris.task, method = "information.gain", abs = 2)
+# Keep the 25% most important features
+filtered.task = filterFeatures(iris.task, fval = fv, perc = 0.25)
+# Keep all features with importance greater than 0.5
+filtered.task = filterFeatures(iris.task, fval = fv, threshold = 0.5)
+filtered.task
 
 
 
